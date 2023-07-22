54b2b90fd9e177c5599dd1da876cea63df05f781
Meisam Mulla <mmeisam@gmail.com>
2023-07-22 19:11:13 -0400

    Write to static file

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,18 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'GoogleAddressAutocomplete',
+      fileName: 'index',
+    },
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]'
+      }
+    }
+  }
+});
