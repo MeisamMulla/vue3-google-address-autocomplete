017e4d11121ef253fa53fa9d54c5ee54a3415aa7
Meisam Mulla <mmeisam@gmail.com>
2023-07-23 11:59:27 -0400

    rewrite using defineComponent

--- src/components/GoogleAddressAutocomplete.vue
+++ src/components/GoogleAddressAutocomplete.vue
@@ -1,77 +1,85 @@
 <template>
-    <input 
-        :ref="(autocompleteInput as any)" 
-        type="text" 
-        autocomplete="off" 
-        :placeholder="placeholder" 
+    <input
+        ref="autocompleteInput"
+        type="text"
+        autocomplete="off"
+        :placeholder="placeholder"
         :class="class"
     />
 </template>
-  
-<script setup lang="ts">
+
+<script lang="ts">
 import { ref, onMounted, onBeforeUnmount } from "vue";
+import { defineComponent } from 'vue';
 
-const props = defineProps<{
-    modelValue?: string;
-    apiKey: string;
-    placeholder?: string;
-    class?: string;
-}>();
+export default defineComponent({
+    props: {
+        modelValue: String,
+        apiKey: String,
+        placeholder: String,
+        class: String
+    },
+    emits: ["update:modelValue", "callback"],
+    setup(props, { emit }) {
+        const autocompleteInput = ref<HTMLInputElement | null>(null);
+        let scriptLoaded = false;
 
-const emit = defineEmits<{
-    (e: "update:modelValue", value: string): void;
-    (e: "callback", value: object): void;
-}>();
+        function loadGoogleMapsScript() {
+            return new Promise<void>((resolve, reject) => {
+                if (window.google && window.google.maps && window.google.maps.places) {
+                    resolve();
+                } else {
+                    if (!scriptLoaded) {
+                        scriptLoaded = true;
+                        const googleMapsScript = document.createElement("script");
+                        googleMapsScript.setAttribute(
+                            "src",
+                            `https://maps.googleapis.com/maps/api/js?key=${props.apiKey}&libraries=places&callback=initMap`
+                        );
 
-let autocompleteInput = ref<HTMLInputElement | null>(null);
-let scriptLoaded = false;
+                        window.initMap = () => {
+                            resolve();
+                        };
 
-// Load the Google Maps API script
-function loadGoogleMapsScript() {
-    return new Promise<void>((resolve, reject) => {
-        if (window.google && window.google.maps && window.google.maps.places) {
-            resolve();
-        } else {
-            if (!scriptLoaded) {
-                scriptLoaded = true;
-                const googleMapsScript = document.createElement("script");
-                googleMapsScript.setAttribute(
-                    "src",
-                    `https://maps.googleapis.com/maps/api/js?key=${props.apiKey}&libraries=places&callback=initMap`
+                        googleMapsScript.onerror = (error) => {
+                            reject(error);
+                        };
+
+                        document.head.appendChild(googleMapsScript);
+                    }
+                }
+            });
+        }
+
+        onMounted(async () => {
+            try {
+                await loadGoogleMapsScript();
+                setupAutocomplete();
+            } catch (error) {
+                console.error("Failed to load Google Maps API", error);
+            }
+        });
+
+        onBeforeUnmount(() => {
+            delete window.initMap;
+        });
+
+        function setupAutocomplete() {
+            if (autocompleteInput.value) {
+                const autocomplete = new google.maps.places.Autocomplete(
+                    autocompleteInput.value
                 );
 
-                window.initMap = () => {
-                    resolve();
-                };
+                autocomplete.addListener("place_changed", () => {
+                    const place = autocomplete.getPlace();
 
-                googleMapsScript.onerror = (error) => {
-                    reject(error);
-                };
-
-                document.head.appendChild(googleMapsScript);
+                    emit("update:modelValue", place.formatted_address);
+                    emit("callback", place);
+                });
             }
         }
-    });
-}
 
-onMounted(async () => {
-    try {
-        await loadGoogleMapsScript();
-        const autocomplete = new google.maps.places.Autocomplete(
-            autocompleteInput.value
-        );
-        autocomplete.addListener("place_changed", () => {
-            const place = autocomplete.getPlace();
-            emit("update:modelValue", place.formatted_address);
-            emit("callback", place);
-        });
-    } catch (error) {
-        console.error("Failed to load Google Maps API", error);
-    }
-});
-
-onBeforeUnmount(() => {
-    // Remove the global callback to clean up
-    delete window.initMap;
+        return { autocompleteInput };
+    },
 });
 </script>
